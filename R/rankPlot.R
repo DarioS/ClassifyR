3a9ea5fed89d737350e71adf0e11fb996a685c5b
Dario Strbenac <D.Strbenac@garvan.org.au>
2014-11-20 06:00:32 +0000

    rankStabilityPlot changed to rankPlot. Now does both stability and commonality between analyses. More options for plot margins and labels.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ClassifyR@96955 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/rankPlot.R
@@ -0,0 +1,112 @@
+setGeneric("rankPlot", function(results, ...)
+{standardGeneric("rankPlot")})
+
+setMethod("rankPlot", "list", 
+          function(results, topRanked = seq(10, 100, 10),
+                   comparison = c("within", "classificationName", "validation", "datasetName"),
+                   lineColourVariable = c("validation", "datasetName", "classificationName", "None"),
+                   pointTypeVariable = c("datasetName", "classificationName", "validation", "None"),
+                   rowVariable = c("None", "datasetName", "classificationName", "validation"),
+                   columnVariable = c("classificationName", "datasetName", "validation", "None"),
+                   yMax = 100, fontSizes = c(24, 16, 12, 12, 12), title = "Feature Ranking Stability",
+                   xLabelPositions = seq(10, 100, 10), yLabel = "Average Pairwise Common Features (%)",
+                   plot = TRUE, parallelParams = bpparam())
+{
+  if(!requireNamespace("ggplot2", quietly = TRUE))
+    stop("The package 'ggplot2' could not be found. Please install it.")       
+  
+  comparison <- match.arg(comparison)
+  lineColourVariable <- match.arg(lineColourVariable)
+  pointTypeVariable <- match.arg(pointTypeVariable)
+  rowVariable <- match.arg(rowVariable)
+  columnVariable <- match.arg(columnVariable)
+  
+  results <- lapply(results, function(result) # Unlist the folds of Resample and Fold.
+  {
+    if(is.list(result@rankedFeatures[[1]]))
+      result@rankedFeatures <- unlist(result@rankedFeatures, recursive = FALSE)
+    result
+  })
+  
+  if(comparison == "within")
+  {
+    plotData <- do.call(rbind, bplapply(results, function(result)
+    {
+      rankedFeatures <- result@rankedFeatures
+      averageOverlap <- rowMeans(do.call(cbind, mapply(function(features, index)
+      {
+        otherFeatures <- rankedFeatures[(index + 1):length(rankedFeatures)]
+        sapply(otherFeatures, function(other)
+        {
+          sapply(topRanked, function(top)
+          {
+            length(intersect(features[1:top], other[1:top])) / top * 100
+          })
+        })
+      }, rankedFeatures[1:(length(rankedFeatures) - 1)], 1:(length(rankedFeatures) - 1), SIMPLIFY = FALSE)))
+      
+      data.frame(dataset = rep(result@datasetName, length(topRanked)),
+                 analysis = rep(result@classificationName, length(topRanked)),
+                 validation = rep(result@validation[[1]], length(topRanked)),
+                 top = topRanked,
+                 overlap = averageOverlap)
+    }, BPPARAM = parallelParams))
+  } else { # Commonality analysis.
+    analyses <- sapply(results, function(result) result@classificationName) 
+    validations <- sapply(results, function(result) paste(unlist(result@validation), collapse = " "))
+    datasets <- sapply(results, function(result) result@datasetName)
+
+    if(comparison == "classificationName")
+      compareIndices <- split(1:length(results), paste(validations, datasets, sep = " "))
+    else if(comparison == "validation")
+      compareIndices <- split(1:length(results), paste(analyses, datasets, sep = " "))
+    else
+      compareIndices <- split(1:length(results), paste(analyses, validations, sep = " "))
+
+    plotData <- do.call(rbind, unname(unlist(bplapply(compareIndices, function(indicesSet)
+    {
+      unname(lapply(data.frame(combn(indicesSet, length(indicesSet))), function(indiciesCombination)
+      {
+        aDataset <- results[[indiciesCombination[1]]]
+        otherDatasets <- results[indiciesCombination[-1]]   
+        averageOverlap <- rowMeans(do.call(cbind, lapply(aDataset@rankedFeatures, function(features)
+        {
+          do.call(cbind, unname(lapply(otherDatasets, function(anotherDataset)
+          {
+            sapply(anotherDataset@rankedFeatures, function(otherFeatures)
+            {
+              sapply(topRanked, function(top)
+              {
+                length(intersect(features[1:top], otherFeatures[1:top])) / top * 100
+              })          
+            })
+          })))
+        })))
+        data.frame(dataset = rep(aDataset@datasetName, length(topRanked)),
+                   analysis = rep(aDataset@classificationName, length(topRanked)),
+                   validation = rep(aDataset@validation[[1]], length(topRanked)),
+                   top = topRanked,
+                   overlap = averageOverlap)
+      }))
+    }, BPPARAM = parallelParams), recursive = FALSE)))
+  }
+  
+  plotData[, "validation"] <- gsub("fold", "Resample and Fold", plotData[, "validation"])
+  plotData[, "validation"] <- gsub("split", "Resample and Split", plotData[, "validation"])
+  plotData[, "validation"] <- gsub("leave", "Leave Out", plotData[, "validation"])
+
+  overlapPlot <- ggplot2::ggplot(plotData, ggplot2::aes(x = top, y = overlap,
+                          colour = switch(lineColourVariable, validation = validation, datasetName = dataset, classificationName = analysis),
+                          shape = switch(pointTypeVariable, validation = validation, datasetName = dataset, classificationName = analysis)), environment = environment()) +
+                          ggplot2::geom_line() + ggplot2::geom_point(size = 3) + ggplot2::scale_x_continuous(breaks = xLabelPositions) + ggplot2::scale_y_continuous(limits = c(0, yMax)) + ggplot2::xlab("Top Features") + ggplot2::ylab(yLabel) +
+                          ggplot2::ggtitle(title) + ggplot2::labs(colour = switch(lineColourVariable, validation = "Validation", datasetName = "Dataset", classificationName = "Analysis"), shape = switch(pointTypeVariable, validation = "Validation", datasetName = "Dataset", classificationName = "Analysis")) +
+                          ggplot2::theme(axis.title = ggplot2::element_text(size = fontSizes[2]), axis.text = ggplot2::element_text(colour = "black", size = fontSizes[3]), legend.title = ggplot2::element_text(size = fontSizes[4]), legend.text = ggplot2::element_text(size = fontSizes[5]), plot.title = ggplot2::element_text(size = fontSizes[1]), plot.margin = grid::unit(c(0, 0, 1, 0), "lines"), legend.margin = grid::unit(-1, "lines"))
+  
+  if(rowVariable != "None" || columnVariable != "None")
+    overlapPlot <- overlapPlot + ggplot2::facet_grid(paste(if(rowVariable != "None") switch(rowVariable, validation = "validation", datasetName = "dataset", classificationName = "analysis"), "~", if(columnVariable != "None") switch(columnVariable, validation = "validation", datasetName = "dataset", classificationName = "analysis")))
+  
+  if(plot == TRUE)
+    print(overlapPlot)
+  
+  overlapPlot
+})
---
+++ R/selectionPlot.R
@@ -0,0 +1,112 @@
+setGeneric("selectionPlot", function(results, ...)
+{standardGeneric("selectionPlot")})
+
+setMethod("selectionPlot", "list", 
+          function(results,
+                   comparison = c("within", "classificationName", "validation", "datasetName"),
+                   xVariable = c("classificationName", "datasetName", "validation"),
+                   boxFillColouring = c("classificationName", "datasetName", "validation", "None"),
+                   boxFillColours = NULL,
+                   boxLineColouring = c("validation", "classificationName", "validation", "None"),
+                   boxLineColours = NULL,
+                   rowVariable = c("None", "validation", "datasetName", "classificationName"),
+                   columnVariable = c("datasetName", "classificationName", "validation", "None"),
+                   yMax = 100, fontSizes = c(24, 16, 12), title = "Feature Selection Stability",
+                   xLabel = "Analysis", yLabel = "Average Pairwise Common Features (%)",
+                   margin = grid::unit(c(0, 1, 1, 0), "lines"), plot = TRUE, parallelParams = bpparam())
+{
+  if(!requireNamespace("ggplot2", quietly = TRUE))
+    stop("The package 'ggplot2' could not be found. Please install it.")             
+  if(!requireNamespace("scales", quietly = TRUE))
+    stop("The package 'scales' could not be found. Please install it.")             
+
+  xVariable <- match.arg(xVariable)
+  boxFillColouring <- match.arg(boxFillColouring)
+  boxLineColouring <- match.arg(boxLineColouring)
+  rowVariable <- match.arg(rowVariable)
+  columnVariable <- match.arg(columnVariable)
+
+  results <- lapply(results, function(result) # Unlist the folds of Resample and Fold.
+  {
+    if(is.list(result@chosenFeatures[[1]]))
+      result@chosenFeatures <- unlist(result@chosenFeatures, recursive = FALSE)
+    result
+  })
+  
+  if(comparison == "within")
+  {  
+    plotData <- do.call(rbind, bplapply(results, function(result)
+    {
+  
+      percentOverlaps <- unlist(mapply(function(features, index)
+      {
+        otherFeatures <- chosenFeatures[(index + 1):length(chosenFeatures)]
+        sapply(otherFeatures, function(other)
+        {
+          length(intersect(features, other)) / max(length(features), length(other)) * 100
+        })
+      }, chosenFeatures[1:(length(chosenFeatures) - 1)], 1:(length(chosenFeatures) - 1), SIMPLIFY = FALSE))
+      
+      data.frame(dataset = rep(result@datasetName, length(percentOverlaps)),
+                 analysis = rep(result@classificationName, length(percentOverlaps)),
+                 validation = rep(result@validation[1], length(percentOverlaps)),
+                 overlap = percentOverlaps)
+    }, BPPARAM = parallelParams))
+  } else { # Commonality analysis.
+    analyses <- sapply(results, function(result) result@classificationName) 
+    validations <- sapply(results, function(result) paste(unlist(result@validation), collapse = " "))
+    datasets <- sapply(results, function(result) result@datasetName)
+    
+    if(comparison == "classificationName")
+      compareIndices <- split(1:length(results), paste(validations, datasets, sep = " "))
+    else if(comparison == "validation")
+      compareIndices <- split(1:length(results), paste(analyses, datasets, sep = " "))
+    else
+      compareIndices <- split(1:length(results), paste(analyses, validations, sep = " "))
+    
+    plotData <- do.call(rbind, unname(unlist(lapply(compareIndices, function(indicesSet)
+    {
+      unname(lapply(data.frame(combn(indicesSet, length(indicesSet))), function(indiciesCombination)
+      {
+        aDataset <- results[[indiciesCombination[1]]]
+        otherDatasets <- results[indiciesCombination[-1]]   
+        allOverlaps <- unlist(lapply(aDataset@chosenFeatures, function(features)
+        {
+          sapply(otherDatasets, function(anotherDataset)
+          {
+            sapply(anotherDataset@chosenFeatures, function(otherFeatures)
+            {
+              length(intersect(features, otherFeatures)) / max(length(features), length(otherFeatures)) * 100
+            })
+          })
+        }))
+
+        data.frame(dataset = rep(aDataset@datasetName, length(allOverlaps)),
+                   analysis = rep(aDataset@classificationName, length(allOverlaps)),
+                   validation = rep(aDataset@validation[[1]], length(allOverlaps)),
+                   overlap = allOverlaps)
+      }))
+    }), recursive = FALSE)))
+  }
+
+  if(boxFillColouring != "None")
+    if(!is.null(boxFillColours)) boxFillColours else boxFillColours <- scales::hue_pal()(switch(boxFillColouring, validation = length(unique(plotData[, "validation"])), datasetName = length(unique(plotData[, "dataset"])), classificationName = length(unique(plotData[, "analysis"]))))
+  if(boxLineColouring != "None")
+    if(!is.null(boxLineColours)) boxLineColours else boxLineColours <- scales::hue_pal(direction = -1)(switch(boxLineColouring, validation = length(unique(plotData[, "validation"])), datasetName = length(unique(plotData[, "dataset"])), classificationName = length(unique(plotData[, "analysis"]))))
+  plotData[, "validation"] <- gsub("fold", "Resample and Fold", plotData[, "validation"])
+  plotData[, "validation"] <- gsub("split", "Resample and Split", plotData[, "validation"])
+  plotData[, "validation"] <- gsub("leave", "Leave Out", plotData[, "validation"])
+  
+  selectionPlot <- ggplot2::ggplot(plotData, ggplot2::aes(x = switch(xVariable, validation = validation, datasetName = dataset, classificationName = analysis), y = overlap,
+                          fill = switch(boxFillColouring, validation = validation, datasetName = dataset, classificationName = analysis, None = NULL), colour = switch(boxLineColouring, validation = validation, datasetName = dataset, classificationName = analysis, None = NULL)), environment = environment()) +
+                          ggplot2::geom_boxplot() + ggplot2::scale_y_continuous(limits = c(0, yMax)) + ggplot2::scale_fill_manual(values = boxFillColours) + ggplot2::scale_colour_manual(values = boxLineColours) + ggplot2::xlab(xLabel) + ggplot2::ylab(yLabel) +
+                          ggplot2::ggtitle(title) + ggplot2::theme(legend.position = "none", axis.title = ggplot2::element_text(size = fontSizes[2]), axis.text = ggplot2::element_text(colour = "black", size = fontSizes[3]), plot.title = ggplot2::element_text(size = fontSizes[1]), plot.margin = margin)
+
+  if(rowVariable != "None" || columnVariable != "None")
+    selectionPlot <- selectionPlot + ggplot2::facet_grid(paste(if(rowVariable != "None") switch(rowVariable, validation = "validation", datasetName = "dataset", classificationName = "analysis"), "~", if(columnVariable != "None") switch(columnVariable, validation = "validation", datasetName = "dataset", classificationName = "analysis")))
+  
+  if(plot == TRUE)
+    print(selectionPlot)
+  
+  selectionPlot
+})
