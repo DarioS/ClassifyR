48cc747b01aadc66f16b884816774bb141de56dc
Dario Strbenac <D.Strbenac@garvan.org.au>
2015-01-07 01:00:10 +0000

    graph type argument for plotFeatureClasses is renamed whichPlots. plot is now used to specify whether a plot is drawn on the current device.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ClassifyR@98094 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotFeatureClasses.R
+++ R/plotFeatureClasses.R
@@ -14,7 +14,7 @@ setMethod("plotFeatureClasses", "matrix",
 })
 
 setMethod("plotFeatureClasses", "ExpressionSet", 
-          function(expression, rows, plot = c("both", "density", "stripchart"),
+          function(expression, rows, whichPlots = c("both", "density", "stripchart"),
                    xAxisLabel = expression(log[2](expression)), expressionLimits = c(2, 16),
                    yAxisLabels = c("Density", "Classes"),
                    showXtickLabels = TRUE, showYtickLabels = TRUE,
@@ -27,21 +27,21 @@ setMethod("plotFeatureClasses", "ExpressionSet",
   if(!requireNamespace("gridExtra", quietly = TRUE))
     stop("The package 'gridExtra' could not be found. Please install it.")     
 
-  plot <- match.arg(plot)
+  whichPlots <- match.arg(whichPlots)
   classes <- pData(expression)[, "class"]
   features <- featureNames(expression)
   expression <- exprs(expression)
   invisible(lapply(rows, function(featureRow)
   {
     plotData <- data.frame(expr = expression[featureRow, ], classes)
-    if(plot %in% c("both", "density"))
+    if(whichPlots %in% c("both", "density"))
     {
       densPlot <- ggplot2::ggplot(plotData, ggplot2::aes(x = expr, colour = classes)) +
         ggplot2::stat_density(ggplot2::aes(y = ..density..), geom = "path", position = "identity", size = 1) +
         ggplot2::scale_colour_manual("Class", values = c("red", "blue")) +
         ggplot2::scale_x_continuous(limits = expressionLimits)
     }
-    if(plot %in% c("both", "stripchart"))
+    if(whichPlots %in% c("both", "stripchart"))
     {
       stripPlot <- ggplot2::ggplot(plotData, ggplot2::aes(x = classes, y = expr)) +
                    ggplot2::geom_dotplot(dotsize = 0.5, binaxis = "y", stackdir = "center", position = "dodge", ggplot2::aes(colour = classes)) +
@@ -54,7 +54,7 @@ setMethod("plotFeatureClasses", "ExpressionSet",
       stripPlot <- stripPlot + ggplot2::coord_flip()
     }
     if(is.numeric(rows)) featureName <- features[featureRow] else featureName <-featureRow
-    if(plot == "both")
+    if(whichPlots == "both")
     {
       densPlot <- densPlot + ggplot2::theme(axis.title.x = ggplot2::element_blank(), axis.line = ggplot2::element_blank(),
                                             axis.text.x = if(showXtickLabels == TRUE) ggplot2::element_text(size = fontSizes[3]) else ggplot2::element_blank(),
@@ -67,7 +67,7 @@ setMethod("plotFeatureClasses", "ExpressionSet",
       if(plot == TRUE)
         print(bothGraphics)
       bothGraphics
-    } else if(plot == "density")
+    } else if(whichPlots == "density")
     {
       densPlot <- densPlot + ggplot2::lab(x = xAxisLabel, y = yAxisLabels[1]) +
                   ggplot2::theme(plot.title = ggplot2::element_text(size = fontSizes[1]),
