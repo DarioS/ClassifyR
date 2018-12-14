c87f92c3c4598cd03617d378bfebb75edcfb589a
Dario Strbenac <dstr7320@uni.sydney.edu.au>
2018-12-14 11:15:15 +1100

    - Removed accidentally commited browser() from elasticNetFeatures function. - Better error message emitted by edgesToSubnetworks when there are no subnetworks with at least the minimum number of features.

--- R/elasticNetFeatures.R
+++ R/elasticNetFeatures.R
@@ -4,8 +4,7 @@ setGeneric("elasticNetFeatures", function(model, ...)
 setMethod("elasticNetFeatures", "multnet",
           function(model)
 {
-  inputFeatures <- attr(model, "features")
-  browser()          
+  inputFeatures <- attr(model, "features") 
   # Floating point numbers test for equality.
   whichCoefficientColumn <- which(abs(model[["lambda"]] - attr(model, "tune")[["lambda"]]) < 0.00001)[1]
   coefficientsUsed <- sapply(model[["beta"]], function(classCoefficients) classCoefficients[, whichCoefficientColumn])
