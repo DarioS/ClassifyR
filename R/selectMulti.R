e52f61455275ca7a7021dcb22093ac2cef5bc098
Ellis Patrick <epatrick87@gmail.com>
2022-01-13 07:38:17 +1100

    fixed selectMulti

--- R/selectMulti.R
+++ R/selectMulti.R
@@ -6,18 +6,17 @@ setMethod("selectMulti", "DataFrame",
           function(measurements, classes, params, verbose = 0)
           {
               
-              assayTrain <- split(data.frame(t(as.data.frame(measurements))), mcols(measurements)[["dataset"]])
-              assayTrain <- lapply(assayTrain, t)
-              assayTrain <- lapply(assayTrain, DataFrame)
+              assayTrain <- sapply(unique(mcols(measurements)[["dataset"]]), function(x) measurements[,mcols(measurements)[["dataset"]]%in%x], simplify = FALSE)
               
               selectedFeatures <- mapply(ClassifyR:::.doSelection, 
                                          measurements = assayTrain,
                                          modellingParams = params[names(assayTrain)],
                                          MoreArgs = list(classes = classes, 
                                                          training = seq_len(nrow(assayTrain[[1]])),
-                                                         crossValParams = crossValParams,
+                                                         crossValParams = CrossValParams(permutations = 1, folds = 5), ###### Where to get this from?
                                                          verbose = 0)
               )
               
-              DataFrame(dataset = rep(names(selectedFeatures[2,]), unlist(lapply(selectedFeatures[2,], length))), feature = unlist(selectedFeatures[2,]))
+              do.call("rbind", selectedFeatures[2,])
+              #DataFrame(dataset = rep(names(selectedFeatures[2,]), unlist(lapply(selectedFeatures[2,], length))), feature = unlist(selectedFeatures[2,]))
           })
