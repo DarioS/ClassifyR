2fb08dd4ce8127e235cacaaa50e700d45bb0bbb3
Alexander Berrell <unknownuser@192-168-1-112.tpgi.com.au>
2022-02-12 22:01:36 +1100

    Testing addition of NEMOE

--- R/constants.R
+++ R/constants.R
@@ -1,5 +1,6 @@
 .ClassifyRenvir <- new.env(parent = emptyenv())
 
+<<<<<<< HEAD
 .ClassifyRenvir[["performanceInfoTable"]] <- data.frame(type = c("Error", "Accuracy", "Balanced Error", "Balanced Accuracy",
                                             "Micro Precision", "Micro Recall",
                                             "Micro F1", "Macro precision",
@@ -31,3 +32,61 @@
               "Random Forest", "Random Forest", "Location Subtraction", "Support Vector Machine",
               "Support Vector Machine", "Cox proportional hazards", "Cox proportional hazards", "Cox proportional hazards", "Penalised cox proportional hazards", "Penalised cox proportional hazards", "NEMoE", "NEMoE")
               )
+=======
+# Used internally during parameter selection based on best performance.
+.ClassifyRenvir[["performanceInfoTable"]] <- matrix(c("Error", "lower",
+                                                      "Accuracy", "higher",
+                                                      "Balanced Error", "lower",
+                                                      "Balanced Accuracy", "higher",
+                                                      "Micro Precision", "higher",
+                                                      "Micro Recall", "higher",
+                                                      "Micro F1", "higher",
+                                                      "Macro Precision", "higher",
+                                                      "Macro Recall", "higher",
+                                                      "Macro F1", "higher",
+                                                      "Matthews Correlation Coefficient", "higher",
+                                                      "C-index", "higher"),
+                                                    ncol = 2, byrow = TRUE, dimnames = list(NULL, c("type", "better"))
+                                                    ) |> as.data.frame()
+
+# Nice-looking names for feature selection and classification functions, to automatically use
+# in a variety of performance plots.
+.ClassifyRenvir[["functionsTable"]] <- matrix(
+                            c("bartlettRanking", "Bartlett Test",
+                            "classifyInterface", "Poisson LDA",
+                            "differentMeansRanking", "Difference in Means",
+                            "DLDAtrainInterface", "Diagonal LDA",
+                            "DLDApredictInterface", "Diagonal LDA",
+                            "DMDranking", "Differences of Medians and Deviations",
+                            "edgeRranking", "edgeR LRT",
+                            "elasticNetGLMtrainInterface", "Elastic Net GLM",
+                            "elasticNetGLMpredictInterface", "Elastic Net GLM",
+                            "fisherDiscriminant", "Fisher's LDA",
+                            "kNNinterface", "k Nearest Neighbours",
+                            "KolmogorovSmirnovRanking", "Kolmogorov-Smirnov Test",
+                            "kTSPclassifier", "k Top-Scoring Pairs",
+                            "KullbackLeiblerRanking", "Kullback-Leibler Divergence",
+                            "leveneRanking", "Levene Test",
+                            "likelihoodRatioRanking", "Likelihood Ratio Test (Normal)",
+                            "limmaRanking", "Moderated t-test",
+                            "mixModelsTrain", "Mixtures of Normals",
+                            "mixModelsPredict", "Mixtures of Normals",
+                            "naiveBayesKernel", "Naive Bayes Kernel",
+                            "NSCtrainInterface",  "Nearest Shrunken Centroids",
+                            "NSCpredictInterface", "Nearest Shrunken Centroids",
+                            "pairsDifferencesRanking", "Pairs Differences",
+                            "previousSelection", "Previous Selection", 
+                            "previousTrained", "Previous Trained",
+                            "randomForestTrainInterface", "Random Forest",
+                            "randomForestPredictInterface", "Random Forest",
+                            "subtractFromLocation", "Location Subtraction",
+                            "SVMtrainInterface", "Support Vector Machine",
+                            "SVMpredictInterface", "Support Vector Machine",
+                            "coxphTrainInterface", "Cox Proportional Hazards",
+                            "coxphPredictInterface", "Cox Proportional Hazards",
+                            "coxphRanking", "Cox Proportional Hazards",
+                            "coxnetTrainInterface", "Penalised Cox Proportional Hazards",
+                            "coxnetPredictInterface", "Penalised Cox Proportional Hazards"),
+                            ncol = 2, byrow = TRUE, dimnames = list(NULL, c("character", "name"))
+              ) |> as.data.frame()
+>>>>>>> a1042463bd17b00e24b9ac6790be4ddda6bdd41d
